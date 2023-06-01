2e5a77f374a49c0873a4a360b2820dc622141e21
bartoszdurda <bartoszdurda@gmail.com>
2023-06-01 08:52:19 +0000

    update#12

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders dart scoreboard header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/DART SCOREBOARD/i);
+  const linkElement = screen.getByText(/DART/i);
   expect(linkElement).toBeInTheDocument();
 });
